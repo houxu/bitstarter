40c5bbbe91e8ae447536d916f91c3c01e1ffbe15
houxianxu <houxu2008hx@163.com>
2013-07-06 18:47:56 +0800

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("./index.html", "utf8").toString());
+  response.send(fs.readFileSync("张庆斌 and 侯贤旭").toString());
 });
 
 var port = process.env.PORT || 5000;
